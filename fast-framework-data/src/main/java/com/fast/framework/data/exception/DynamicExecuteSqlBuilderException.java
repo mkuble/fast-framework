eec8d60cf4b9a486ccc111ab57ba23aa04736989
lion.chen <lionchen@cao-dao.com>
2017-12-21 18:06:09 +0800

    添加注释

--- fast-framework-data/src/main/java/com/fast/framework/data/exception/DynamicExecuteSqlBuilderException.java
+++ fast-framework-data/src/main/java/com/fast/framework/data/exception/DynamicExecuteSqlBuilderException.java
@@ -1,5 +1,10 @@
 package com.fast.framework.data.exception;
 
+/**
+ * 动态构建参数异常
+ * @author lion.chen
+ *
+ */
 public class DynamicExecuteSqlBuilderException extends RuntimeException {
 
 	private static final long serialVersionUID = -8347468875156268518L;
--- fast-framework-data/src/main/java/com/fast/framework/data/exception/SQLTemplateException.java
+++ fast-framework-data/src/main/java/com/fast/framework/data/exception/SQLTemplateException.java
@@ -1,5 +1,10 @@
 package com.fast.framework.data.exception;
 
+/**
+ * SQL模板异常
+ * @author lion.chen
+ *
+ */
 public class SQLTemplateException extends RuntimeException{
 
 	private static final long serialVersionUID = 8831244893422176865L;
